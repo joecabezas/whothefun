932d51025b84d9cf2e4a4fecc766d5c811185f40
Joe Cabezas Campos <joe.cabezas@gmail.com>
2021-01-31 00:34:22 -0800

    init app

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,7 @@
 const path = require('path');
 const webpack = require('webpack');
 
-
-
-
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-
-
-
 
 /*
  * We've enabled HtmlWebpackPlugin for you! This generates a html
