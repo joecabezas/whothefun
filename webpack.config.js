125018a9e8ff5c57221dcd980af8462ba82b581e
Joe Cabezas Campos <joe.cabezas@gmail.com>
2021-01-30 19:32:36 -0800

    npx webpack init

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const webpack = require('webpack');
+
+
+
+
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+
+
+
+/*
+ * We've enabled HtmlWebpackPlugin for you! This generates a html
+ * page for you when you compile webpack, which will make you start
+ * developing and prototyping faster.
+ *
+ * https://github.com/jantimon/html-webpack-plugin
+ *
+ */
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.ts',
+
+  plugins: [new webpack.ProgressPlugin(), new HtmlWebpackPlugin({
+            template: 'index.html'
+          })],
+
+  module: {
+    rules: [{
+      test: /\.(ts|tsx)$/,
+      loader: 'ts-loader',
+      include: [path.resolve(__dirname, 'src')],
+      exclude: [/node_modules/]
+    }]
+  },
+
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  },
+
+  devServer: {
+    open: true,
+    host: 'localhost'
+  }
+}
