cca6638361fd369327a45963fc47589894073f4f
Joe Cabezas Campos <joe.cabezas@gmail.com>
2021-01-31 01:47:03 -0800

    hello world

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const App = ({}) => <div>hola</div>;
+
+export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const webpack = require('webpack');
 
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 /*
  * We've enabled HtmlWebpackPlugin for you! This generates a html
@@ -14,27 +14,27 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   mode: 'development',
-  entry: './src/index.ts',
+  entry: './src/index.tsx',
 
   plugins: [new webpack.ProgressPlugin(), new HtmlWebpackPlugin({
-            template: 'index.html'
-          })],
+    template: 'index.html',
+  })],
 
   module: {
     rules: [{
       test: /\.(ts|tsx)$/,
       loader: 'ts-loader',
       include: [path.resolve(__dirname, 'src')],
-      exclude: [/node_modules/]
-    }]
+      exclude: [/node_modules/],
+    }],
   },
 
   resolve: {
-    extensions: ['.tsx', '.ts', '.js']
+    extensions: ['.tsx', '.ts', '.js'],
   },
 
   devServer: {
     open: true,
-    host: 'localhost'
-  }
-}
+    host: 'localhost',
+  },
+};
